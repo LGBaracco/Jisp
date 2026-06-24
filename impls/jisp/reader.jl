96b863ebbdd97587c961931581cbf313bba87a86
Lorenzo <lorenzobaracco01@gmail.com>
2026-06-24 17:55:32 +0200

    fixed read_list method

--- impls/jisp/reader.jl
+++ impls/jisp/reader.jl
@@ -1,6 +1,8 @@
 module reader
 export read_str
 
+# TODO just finished the read list, now everything else (included printer)
+
 mutable struct Reader
     position::Int64
     tokens::Array
@@ -10,6 +12,10 @@ function next!(rdr::Reader)
     token = rdr.tokens[rdr.position]
     rdr.position += 1
     token
+end
+
+function peeknext(rdr::Reader)
+    rdr.tokens[rdr.position+1]
 end
 
 function peek(rdr::Reader)
@@ -28,32 +34,34 @@ function read_str(input::String)
 end
 
 function tokenize(input::String)
-    [join(m.match) for m in eachmatch(r"[\s,]*(~@|[\[\]{}()'`~^@]|\"(?:\\.|[^\"])*\"?|;.*|[^\s\[\]{}('\"`,;)]*)", input)]
+    [join(strip(m.match, ' ')) for m in eachmatch(r"[\s,]*(~@|[\[\]{}()'`~^@]|\"(?:\\.|[^\"])*\"?|;.*|[^\s\[\]{}('\"`,;)]*)", input)]
 end
 
 function read_form(rdr::Reader)
     if peek(rdr) == "("
+
         read_list(rdr)
     else
         read_atom(rdr)
     end
 end
 
+a = read_str("( 1 2 3 )")
+
 function read_list(rdr::Reader)
     parsed = []
-    token = next!(rdr)
-    while token != ")"
+    while peeknext(rdr) != ")"
+        next!(rdr)
         push!(parsed, read_form(rdr))
     end
-    next!(rdr)
     parsed
 end
 
 function read_atom(rdr::Reader)
     if match(r"^\d+$", peek(rdr)) !== nothing
-        return parse(Int, peek(rdr))
+        parse(Int, peek(rdr))
     elseif match(r"^[+\-*/]$", peek(rdr)) !== nothing
-        return Symbol(peek(rdr))
+        symbol(peek(rdr))
     end
 end
 
